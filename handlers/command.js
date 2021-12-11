4817afca60adc92f1719d9f524dc04d5595be366
BerryGod <84345559+BerryGod@users.noreply.github.com>
2021-12-12 00:05:24 +0100

    Update command.js

--- handlers/command.js
+++ handlers/command.js
@@ -5,7 +5,7 @@ const ascii = require("ascii-table");
 let table = new ascii("Parancsok");
 
 
-module.exports = (client) => {
+module.exports = (bot) => {
     readdirSync("./commands/").forEach(dir => {
         const commands = readdirSync(`./commands/${dir}/`).filter(file => file.endsWith(".js"));
 
@@ -13,14 +13,14 @@ module.exports = (client) => {
             let pull = require(`../commands/${dir}/${file}`);
 
             if(pull.name){
-                client.commands.set(pull.name, pull)
+                bot.commands.set(pull.name, pull)
                 table.addRow(file, 'Működik');
             } else {
                 table.addRow(file, 'Hiba');
                 continue;
             }
 
-            if(pull.aliases && Array.isArray(pull.aliases)) pull.aliases.forEach(alias => client.aliases.set(alias, pull.name));
+            if(pull.aliases && Array.isArray(pull.aliases)) pull.aliases.forEach(alias => bot.aliases.set(alias, pull.name));
         }
     });
 
